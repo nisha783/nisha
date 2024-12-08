04696a312d013d096c7b12d7127ff0615854b580
nisha783 <hm6980097@gmail.com>
2024-12-08 17:39:40 +0500

    watspp link update

--- resources/views/about/index.blade.php
+++ resources/views/about/index.blade.php
@@ -27,7 +27,7 @@
                 </a>
                 <i class="bi bi-telephone p-2 fs-5" style="color: orange;"></i>
                 <span>Call Us on make on</span>
-                <span style="color: orange;" class="ms-4"><a href="https://wa.me/quiffcuts" style="color: orange;" >https://wa.me/quiffcuts</a></span>
+                <span style="color: orange;" class="ms-4"><a href="https://wa.me/quiffcuts" style="color: orange;" >Whatsapp Contact</a></span>
             </div>
             <div class="right-items">
                 <a href="https://www.facebook.com/quiffcuts1/" target="_blank">
